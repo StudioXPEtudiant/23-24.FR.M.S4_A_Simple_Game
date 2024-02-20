5398bd6c7bede7e41fa64e6291b6951e44766d1f
Artenis67 <artenis67eininger@gmail.com>
2024-02-20 09:29:58 +0100

    Avancement de la scene et des scripts de test

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+@export var SPEED = 100.0
 
 func _physics_process(delta):
-	var direction_x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	var direction_y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	var direction_x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	var direction_y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	
 	velocity.x = direction_x * SPEED
 	velocity.y = direction_y * SPEED
