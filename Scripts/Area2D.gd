bc20e482fb8ab69734bef47c4342c545706109ba
Artenis67 <artenis67eininger@gmail.com>
2024-03-19 18:44:46 +0100

    Ajout du systeme de direction du personnage
    Ajout du systeme de direction du personnage, import de particules

---
+++ Scripts/Area2D.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+@onready var Player = get_parent().get_node("Player")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	if body.get_name() == "Player":
+		queue_free()
+
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -8,5 +8,19 @@ func _physics_process(delta):
 	
 	velocity.x = direction_x * SPEED
 	velocity.y = direction_y * SPEED
+	
+
+func _process(delta):
+	if velocity.length() > 0:
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+		
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_h = velocity.x < 0 # "velocity.x < 0" retrourne la valeur "vrai" ou "faux" ce qui reviens a set une bool
+	elif velocity.y != 0:
+		$AnimatedSprite2D.flip_v = velocity.y > 0
+		$AnimatedSprite2D.animation = "up"
 
 	move_and_slide()
