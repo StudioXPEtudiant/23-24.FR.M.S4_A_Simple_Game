c6b7b7a5cd0e5098125cc55cefac15cf04deae0a
S3rator <145486991+S3rator@users.noreply.github.com>
2024-04-04 19:21:50 +0200

    Levier

---
+++ Scripts/Lever.gd
@@ -0,0 +1,27 @@
+extends Area2D
+
+var player_can_interact = false
+
+func _ready():
+	$AnimatedSprite2D.animation = "Deactivated"
+
+func _unhandled_input(event):
+	if(player_can_interact):
+		if $AnimatedSprite2D.animation == "Deactivated":
+			if event is InputEventKey:
+				if event.pressed and event.keycode == KEY_E :
+					$AnimatedSprite2D.animation = "Activated"
+				
+		elif $AnimatedSprite2D.animation == "Activated":
+			if event is InputEventKey:
+				if event.pressed and event.keycode == KEY_E :
+					$AnimatedSprite2D.animation = "Deactivated"
+
+
+func _on_body_entered(body):
+	$E_key.visible = true
+	player_can_interact = true
+
+func _on_body_exited(body):
+	$E_key.visible = false
+	player_can_interact = false
